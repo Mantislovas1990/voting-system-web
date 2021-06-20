063b6f888611f9bf00e0f13c00fea92468015e91
Mantas <mantislovas24@gmail.com>
2021-06-20 13:54:10 +0300

    solved errors in html and css

--- loginForm/validation.js
+++
@@ -1,9 +0,0 @@
-function nameValidation() {
-    const regName = /^[A-Za-z]+$/;
-    const name = document.getElementById('name').value;
-    if (!regName.test(name)) {
-        alert('Unknown input, please enter only letters');
-        document.getElementById('name').focus();
-        return false;
-    }
-}
